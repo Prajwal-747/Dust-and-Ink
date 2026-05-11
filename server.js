af749cfd261f1177179525f923b5df9fdd18871e
Prajwalmods <prajwalsr28.9.08@gmail.com>
2026-05-11 21:55:02 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -10,30 +10,81 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/random-postcard", async (req, res) => {
+app.post("/ask-ai", async (req, res) => {
   try {
-    const randomPage = Math.floor(Math.random() * 100) + 1;
+    console.log("REQUEST RECEIVED");
 
-    console.log("RANDOM PAGE:");
-    console.log(randomPage);
+    console.log(req.body);
 
-    const response = await fetch(
-      `https://www.loc.gov/pictures/search/?q=city&fo=json&sp=${randomPage}`,
-    );
+    const response = await fetch("https://ai.hackclub.com/proxy/v1/responses", {
+      method: "POST",
 
-    console.log(response.url);
-    console.log(response.status);
+      headers: {
+        Authorization: `Bearer ${process.env.apiKey}`,
+        "Content-Type": "application/json",
+
+        "User-Agent": "Mozilla/5.0",
+        Accept: "application/json",
+      },
+
+      body: JSON.stringify({
+        model: "qwen/qwen3-32b",
+
+        input: [
+          {
+            type: "message",
+
+            role: "user",
+
+            content: [
+              {
+                type: "input_text",
+
+                text: req.body.prompt,
+              },
+            ],
+          },
+        ],
+
+        max_output_tokens: 1000,
+      }),
+    });
+
+    console.log("FETCH RESPONSE:");
+    console.log(response);
 
     const data = await response.json();
 
-    console.log("LOC DATA RECEIVED");
+    console.log("FULL AI DATA:");
+    console.log(data);
 
-    res.json(data);
+    const messageOutput = data.output.find((item) => item.type === "message");
+
+    console.log("MESSAGE OUTPUT:");
+    console.log(messageOutput);
+
+    if (!messageOutput) {
+      console.log("NO MESSAGE OUTPUT FOUND");
+
+      return res.status(500).json({
+        error: "No AI message returned",
+      });
+    }
+
+    const aiText = messageOutput.content[0].text;
+
+    console.log("FINAL AI TEXT:");
+    console.log(aiText);
+
+    res.json({
+      reply: aiText,
+    });
   } catch (error) {
+    console.error("SERVER ERROR:");
     console.error(error);
 
     res.status(500).json({
-      error: "LOC fetch failed",
+      error: "AI Request Failed",
     });
   }
 });
